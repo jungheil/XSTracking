6e3249b51384bd2ea40c2c7efe1bcb5bee90f92b
Xi <jungheilai@qq.com>
2022-01-02 11:58:55 +0800

    update usart

--- src/3rdparty/usart/usart.h
+++ src/3rdparty/usart/usart.h
@@ -16,6 +16,58 @@
 #include <mutex>
 #include <vector>
 
+class CRC16 {
+  public:
+    /*
+    ** Descriptions: CRC16 Verify function
+    ** Input: Data to Verify,Stream length = Data + checksum
+    ** Output: True or False (CRC Verify Result)
+    */
+    bool VerifyCRC16CheckSum(const uint8_t *pchMessage,
+                             const uint32_t dwLength) const;
+    /*
+    ** Descriptions: append CRC16 to the end of data
+    ** Input: Data to CRC and append,Stream length = Data + checksum
+    ** Output: True or False (CRC Verify Result)
+    */
+    bool AppendCRC16CheckSum(uint8_t       *pchMessage,
+                             const uint32_t dwLength) const;
+
+  protected:
+    /*
+    ** Descriptions: CRC16 checksum function
+    ** Input: Data to check,Stream length, initialized checksum
+    ** Output: CRC checksum
+    */
+    virtual uint16_t GetCRC16CheckSum(const uint8_t *pchMessage,
+                                      uint32_t       dwLength) const = 0;
+
+    template <typename T>
+    static T Inverse(const T &buff) {
+        T t;
+        for (int i = 0; i < sizeof(T) * 8; ++i) {
+            t = t << 1;
+            t = t | ((buff >> i) & 0x01);
+        }
+        return std::move(t);
+    }
+};
+
+class CRC16Factory {
+  public:
+    enum CRC16_NAME {
+        CRC16_NAME_IBM        = 0,
+        CRC16_NAME_MAXIM      = 1,
+        CRC16_NAME_USB        = 2,
+        CRC16_NAME_MODBUS     = 3,
+        CRC16_NAME_CCITT      = 4,
+        CRC16_NAME_CCITTFLASE = 5,
+        CRC16_NAME_X25        = 6,
+        CRC16_NAME_XMODEM     = 7
+    };
+    static CRC16 *Get(CRC16_NAME name);
+};
+
 class Usart {
   public:
     Usart() = default;
@@ -30,16 +82,13 @@ class Usart {
     virtual bool Recv(void *msg) { return false; };
 
   private:
-    bool     empty_   = true;
-    bool     is_open_ = false;
-    speed_t  baudr_;
-    char *   device_;
-    int      serial_fd_;  //设备名称
-    uint8_t  len_ = 0;
-    uint16_t GetCRC16CheckSum(uint8_t *pchMessage, uint32_t dwLength,
-                              uint16_t wCRC);
-    uint32_t VerifyCRC16CheckSum(uint8_t *pchMessage, uint32_t dwLength);
-    void     AppendCRC16CheckSum(uint8_t *pchMessage, uint32_t dwLength);
+    bool    empty_   = true;
+    bool    is_open_ = false;
+    speed_t baudr_;
+    char   *device_;
+    int     serial_fd_;  //设备名称
+    uint8_t len_ = 0;
+    CRC16  *crc16;
 };
 
 #endif  // XSTRACKING_USART_H
